050319e59d8ba3203e54f2fb353554adcf9cf34f
Jeremiah85 <jbstoffel@gmail.com>
2022-05-20 10:57:16 -0500

    add to_list function to simplify gui

--- jbs/model/model.py
+++ jbs/model/model.py
@@ -4,3 +4,12 @@ class Model:
         self.set_name = args[1]
         self.artist_name = args[2]
         self.source_name = args[3]
+
+    def to_list(self):
+        result = []
+        result.append(self.model_name)
+        result.append(self.set_name)
+        result.append(self.artist_name)
+        result.append(self.source_name)
+
+        return result
