5300388c4b26d52c049e4249ffaf22fb8c450173
Jeremiah85 <jbstoffel@gmail.com>
2022-05-20 10:36:57 -0500

    refactor models into a class

---
+++ jbs/model/model.py
@@ -0,0 +1,6 @@
+class Model:
+    def __init__(self, args):
+        self.model_name = args[0]
+        self.set_name = args[1]
+        self.artist_name = args[2]
+        self.source_name = args[3]
