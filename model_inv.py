fbb2bccc50a614ec8903fe620a92a5b4746e9275
Jeremiah85 <jbstoffel@gmail.com>
2022-05-24 19:57:17 -0500

    Comment main script

--- model_inv.py
+++ model_inv.py
@@ -1,9 +1,14 @@
 import sys
 import os
+
 import jbs.database.database as db
 import jbs.gui.gui as gui
 
 def main():
+    # This assumes that the database is in the same directory as the script.
+    # TODO: Eventually I want to allow a json or xml or yaml file in the script
+    # directory to explicitly define the database name and location with 
+    # this as a fallback
     scriptpath = os.path.dirname(os.path.realpath(sys.argv[0])) + os.sep
     database = scriptpath + "3D_Models.db"
 
