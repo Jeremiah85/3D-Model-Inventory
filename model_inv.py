b7e4a3d9a3528c3399b1eacb6f0d27b670015a61
Jeremiah85 <jbstoffel@gmail.com>
2022-05-27 08:28:09 -0500

    Merge in config file

--- model_inv.py
+++ model_inv.py
@@ -1,24 +1,29 @@
 import sys
 import os
 
+import jbs.config as config
 import jbs.database.database_utils as db
 import jbs.gui as gui
 
 scriptpath = os.path.dirname(os.path.realpath(sys.argv[0])) + os.sep
-database = scriptpath + "3D_Models.db"
+default_database = scriptpath + "3D_Models.db"
 sql_schema_new = scriptpath + r'sql\empty_database.sql'
+default_config = scriptpath + "config.json"
 
 def main():
-    if os.path.exists(database):
-        con = db.connect_database(database)
+    if os.path.exists(default_database):
+        connection = db.connect_database(default_database)
+    elif os.path.exists(default_config):
+        configuration = config.get_config(default_config)
+        connection = db.connect_database(configuration['database'])
     else:
-        con = db.connect_database(database)
+        connection = db.connect_database(database)
         db.modify_database_schema(con, sql_schema_new)
         
-    app = gui.Window(con)
+    app = gui.Window(connection)
     app.root.mainloop()
 
-    db.close_database(con)
+    db.close_database(connection)
 
 if __name__ == '__main__':
     main()
